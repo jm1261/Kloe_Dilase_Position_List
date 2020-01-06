8095d913fbd926b077d742047543d90c94577429
Josh Male <50483307+jm1261@users.noreply.github.com>
2020-01-06 20:56:45 +0000

    write out function added

--- Functions/InputOutput.py
+++ Functions/InputOutput.py
@@ -9,3 +9,17 @@ def check_dir_exists(dir_path):
     '''
     if os.path.isdir(dir_path) is False:
         os.mkdir(dir_path)
+
+
+def write_out_file(out_path,
+                   array):
+    '''
+    Writes a simpled text file, txt or other extension, based on a given array
+    containing strings. The array entries are written on each line.
+    Args:
+        out_path: <string> directory path to write file
+        array: <array> array containing the out file strings
+    '''
+    output_file = open(f'{out_path}', 'w')
+    [output_file.writelines(f'{L}\n') for L in array]
+    output_file.close()
