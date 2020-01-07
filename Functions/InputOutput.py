683d79449b9625e462e30a001f29aac9ac3be3fb
Josh Male <50483307+jm1261@users.noreply.github.com>
2020-01-07 13:31:49 +0000

    position_list_repeater
    Allows users to repeat a position list in x, y, z directions with modulation and velocity changes

--- Functions/InputOutput.py
+++ Functions/InputOutput.py
@@ -12,8 +12,7 @@ def check_dir_exists(dir_path):
 
 
 def write_out_file(out_path,
-                   array,
-                   pos_i):
+                   array):
     '''
     Writes a simpled text file, txt or other extension, based on a given array
     containing strings. The array entries are written on each line.
@@ -21,13 +20,7 @@ def write_out_file(out_path,
         out_path: <string> directory path to write file
         array: <array> array containing the out file strings
     '''
-    laser_line = pos_i['laser']
-    if laser_line == 10:
-        laser_string = f'"ALS" "Ligne2/Laser1" '
-    elif laser_line == 0.5:
-        laser_string = f'"ALS" "Ligne1/Laser1" '
     output_file = open(f'{out_path}', 'w')
     output_file.writelines('"DflVersion" "3"\n')
-    output_file.writelines(f'{laser_string}\n')
     [output_file.writelines(f'{L}\n') for L in array]
     output_file.close()
--- Functions/PositionBuilder.py
+++ Functions/PositionBuilder.py
@@ -1,226 +1,103 @@
-def x_copy(pos_i,
-           shift):
+def laser_choice(pos_i):
     '''
-    Copy an object with an x-direction shift
+    Selects one of the two laser lines to assign to the position list
     Args:
-        pos_i: <dict> dictionary containing the file type, file
-                          path, modulation, velocity, x-, y- and z- initial
-                          parameters
-        shift: <float/int> step between each x co-ordinate
+        pos_i: <dict> dictionary containing the file type, file path,
+               modulation, velocity, x-, y- and z- initial parameters
+    Returns:
+        laser_string: <string> string to tell Dilase software which laser to
+                      assign to a pattern position list
     '''
-    position_array = []
-    for i in range(0, pos_i['repeat_patterns']):
-        new_x_position = round(pos_i['x_initial'] + (i*shift), 3)
-        position_string = (f'"LWO" '
-                           f'"{pos_i["file_name"]}" '
-                           f'"{pos_i["modulation"]}" '
-                           f'"{pos_i["velocity"]}" '
-                           f'"{new_x_position}" '
-                           f'"{pos_i["y_initial"]}" '
-                           f'"{pos_i["z_initial"]}" ')
-        position_array.append(position_string)
-    return position_array
+    laser_line = pos_i['laser']
+    if laser_line == 10:
+        laser_string = f'"ALS" "Ligne2/Laser1" '
+    elif laser_line == 0.5:
+        laser_string = f'"ALS" "Ligne1/Laser1" '
+    return laser_string
 
 
-def y_copy(pos_i,
-           shift):
+def position_list(pos_i,
+                  shift_array):
     '''
-    Copy an object with an y-direction shift
+    Builds a simple position list. Assigns a laser line, file type, file name,
+    modulation, velocity, x-, y-, z- paramters. Can repeat a single design
+    a number of times in any of the axes, with a modulation and velocity shift.
+    Sets a single position list assigned to a single laser line. Used to repeat
+    a single LWO file.
     Args:
-        pos_i: <dict> dictionary containing the file type, file
-                          path, modulation, velocity, x-, y- and z- initial
-                          parameters
-        shift: <float/int> step between each y co-ordinate
+        pos_i: <dict> dictionary containing the file type, file path,
+               modulation, velocity, x-, y- and z- initial parameters
+        shift_array: <array> array containing the steps in modulation,
+                     velocity, x-, y- and z- position, and number of repeats
+    Returns:
+        position_list: <array> array of strings containing the positions of a
+                       repeated pattern in the Dilase format
     '''
-    position_array = []
-    for i in range(0, pos_i['repeat_patterns']):
-        new_y_position = round(pos_i['y_initial'] + (i*shift), 3)
-        position_string = (f'"LWO" '
-                           f'"{pos_i["file_name"]}" '
-                           f'"{pos_i["modulation"]}" '
-                           f'"{pos_i["velocity"]}" '
-                           f'"{pos_i["x_initial"]}" '
-                           f'"{new_y_position}" '
-                           f'"{pos_i["z_initial"]}" ')
-        position_array.append(position_string)
-    return position_array
+    position_list = []
+    laser_string = laser_choice(pos_i=pos_i)
+    position_list.append(laser_string)
+    for i in range(0, shift_array[5]):
+        file_type = "LWO"
+        file_name = f'"{pos_i["file_name"]}"'
+        modulation = float(pos_i["modulation"])+((i+1)*shift_array[0])
+        velocity = float(pos_i["velocity"])+((i+1)*shift_array[1])
+        x_pos = float(pos_i["x_initial"])+((i+1)*shift_array[2])
+        y_pos = float(pos_i["y_initial"])+((i+1)*shift_array[3])
+        z_pos = float(pos_i["z_initial"])+((i+1)*shift_array[4])
+        position_string = (f'{file_type} '
+                           f'{file_name} '
+                           f'"{modulation}" '
+                           f'"{velocity}" '
+                           f'"{x_pos}" '
+                           f'"{y_pos}" '
+                           f'"{z_pos}" ')
+        position_list.append(position_string)
+    print(f'\nPosition List:\n')
+    [print(L) for L in position_list]
+    return position_list
 
 
-def z_copy(pos_i,
-           shift):
+def repeat_position_list(pos_array,
+                         repeat_array):
     '''
-    Copy an object with an z-direction shift
+    Builds a complete position list from a repeated pattern. Typically used in
+    conjuction with the position_list function. repeat_position_list takes a
+    position list array and repeats the pattern in the x, y or z direction with
+    a potential variation in modulation and velocity. The laser line assigned
+    in the position_list function is not altered here. Used to repeat an array
+    of patterns.
     Args:
-        pos_i: <dict> dictionary containing the file type, file
-                          path, modulation, velocity, x-, y- and z- initial
-                          parameters
-        shift: <float/int> step between each z co-ordinate
+        pos_array: <array> array of strings containing the positions of a
+                   repeated pattern in the Dilase format
+        repeat_array: <array> array containing the steps in modulation,
+                      velocity, x-, y- and z- position, and number of repeats
+    Returns:
+        position_list: <array> array of strings containing the positions of a
+                       repeated pattern in the Dilase format
     '''
-    position_array = []
-    for i in range(0, pos_i['repeat_patterns']):
-        new_z_position = round(pos_i['z_initial'] + (i*shift), 3)
-        position_string = (f'"LWO" '
-                           f'"{pos_i["file_name"]}" '
-                           f'"{pos_i["modulation"]}" '
-                           f'"{pos_i["velocity"]}" '
-                           f'"{pos_i["x_initial"]}" '
-                           f'"{pos_i["y_initial"]}" '
-                           f'"{new_z_position}" ')
-        position_array.append(position_string)
-    return position_array
-
-
-def modulation_x_copy(pos_i,
-                      x_shift,
-                      mod_shift):
-    '''
-    Copy an object with an x-direction shift with a different modulation
-    Args:
-        pos_i: <dict> dictionary containing the file type, file
-                          path, modulation, velocity, x-, y- and z- initial
-                          parameters
-        shift: <float/int> step between each x co-ordinate
-    '''
-    position_array = []
-    for i in range(0, pos_i['repeat_patterns']):
-        new_x_position = round(pos_i['x_initial'] + (i*x_shift), 3)
-        new_modulation = round(pos_i['modulation'] + (i*mod_shift), 1)
-        position_string = (f'"LWO" '
-                           f'"{pos_i["file_name"]}" '
-                           f'"{new_modulation}" '
-                           f'"{pos_i["velocity"]}" '
-                           f'"{new_x_position}" '
-                           f'"{pos_i["y_initial"]}" '
-                           f'"{pos_i["z_initial"]}" ')
-        position_array.append(position_string)
-    return position_array
-
-
-def modulation_y_copy(pos_i,
-                      y_shift,
-                      mod_shift):
-    '''
-    Copy an object with an y-direction shift with a different modulation
-    Args:
-        pos_i: <dict> dictionary containing the file type, file
-                          path, modulation, velocity, x-, y- and z- initial
-                          parameters
-        shift: <float/int> step between each x co-ordinate
-    '''
-    position_array = []
-    for i in range(0, pos_i['repeat_patterns']):
-        new_y_position = round(pos_i['y_initial'] + (i*y_shift), 3)
-        new_modulation = round(pos_i['modulation'] + (i*mod_shift), 1)
-        position_string = (f'"LWO" '
-                           f'"{pos_i["file_name"]}" '
-                           f'"{new_modulation}" '
-                           f'"{pos_i["velocity"]}" '
-                           f'"{pos_i["x_initial"]}" '
-                           f'"{new_y_position}" '
-                           f'"{pos_i["z_initial"]}" ')
-        position_array.append(position_string)
-    return position_array
-
-
-def modulation_z_copy(pos_i,
-                      z_shift,
-                      mod_shift):
-    '''
-    Copy an object with an z-direction shift with a different modulation
-    Args:
-        pos_i: <dict> dictionary containing the file type, file
-                          path, modulation, velocity, x-, y- and z- initial
-                          parameters
-        shift: <float/int> step between each x co-ordinate
-    '''
-    position_array = []
-    for i in range(0, pos_i['repeat_patterns']):
-        new_z_position = round(pos_i['z_initial'] + (i*z_shift), 3)
-        new_modulation = round(pos_i['modulation'] + (i*mod_shift), 1)
-        position_string = (f'"LWO" '
-                           f'"{pos_i["file_name"]}" '
-                           f'"{new_modulation}" '
-                           f'"{pos_i["velocity"]}" '
-                           f'"{pos_i["x_initial"]}" '
-                           f'"{pos_i["y_initial"]}" '
-                           f'"{new_z_position}" ')
-        position_array.append(position_string)
-    return position_array
-
-
-def velocity_x_copy(pos_i,
-                    x_shift,
-                    vel_shift):
-    '''
-    Copy an object with an x-direction shift with a different modulation
-    Args:
-        pos_i: <dict> dictionary containing the file type, file
-                          path, modulation, velocity, x-, y- and z- initial
-                          parameters
-        shift: <float/int> step between each x co-ordinate
-    '''
-    position_array = []
-    for i in range(0, pos_i['repeat_patterns']):
-        new_x_position = round(pos_i['x_initial'] + (i*x_shift), 3)
-        new_velocity = round(pos_i['velocity'] + (i*vel_shift), 1)
-        position_string = (f'"LWO" '
-                           f'"{pos_i["file_name"]}" '
-                           f'"{pos_i["modulation"]}" '
-                           f'"{new_velocity}" '
-                           f'"{new_x_position}" '
-                           f'"{pos_i["y_initial"]}" '
-                           f'"{pos_i["z_initial"]}" ')
-        position_array.append(position_string)
-    return position_array
-
-
-def velocity_y_copy(pos_i,
-                    y_shift,
-                    vel_shift):
-    '''
-    Copy an object with an y-direction shift with a different modulation
-    Args:
-        pos_i: <dict> dictionary containing the file type, file
-                          path, modulation, velocity, x-, y- and z- initial
-                          parameters
-        shift: <float/int> step between each x co-ordinate
-    '''
-    position_array = []
-    for i in range(0, pos_i['repeat_patterns']):
-        new_y_position = round(pos_i['y_initial'] + (i*y_shift), 3)
-        new_velocity = round(pos_i['velocity'] + (i*vel_shift), 1)
-        position_string = (f'"LWO" '
-                           f'"{pos_i["file_name"]}" '
-                           f'"{pos_i["modulation"]}" '
-                           f'"{new_velocity}" '
-                           f'"{pos_i["x_initial"]}" '
-                           f'"{new_y_position}" '
-                           f'"{pos_i["z_initial"]}" ')
-        position_array.append(position_string)
-    return position_array
-
-
-def velocity_z_copy(pos_i,
-                    z_shift,
-                    vel_shift):
-    '''
-    Copy an object with an z-direction shift with a different modulation
-    Args:
-        pos_i: <dict> dictionary containing the file type, file
-                          path, modulation, velocity, x-, y- and z- initial
-                          parameters
-        shift: <float/int> step between each x co-ordinate
-    '''
-    position_array = []
-    for i in range(0, pos_i['repeat_patterns']):
-        new_z_position = round(pos_i['z_initial'] + (i*z_shift), 3)
-        new_velocity = round(pos_i['velocity'] + (i*vel_shift), 1)
-        position_string = (f'"LWO" '
-                           f'"{pos_i["file_name"]}" '
-                           f'"{pos_i["modulation"]}" '
-                           f'"{new_velocity}" '
-                           f'"{pos_i["x_initial"]}" '
-                           f'"{pos_i["y_initial"]}" '
-                           f'"{new_z_position}" ')
-        position_array.append(position_string)
-    return position_array
+    position_list = []
+    [position_list.append(L) for L in pos_array]
+    for i in range(0, repeat_array[5]):
+        for line in pos_array:
+            split = line.split(' ')
+            if split[0] == '"ALS"':
+                position_list.append(line)
+            if split[0] == 'LWO':
+                file_type = split[0]
+                file_name = split[1]
+                modulation = float((split[2])[1:-1])+((i+1)*repeat_array[0])
+                velocity = float((split[3])[1:-1])+((i+1)*repeat_array[1])
+                x_pos = float((split[4])[1:-1])+((i+1)*repeat_array[2])
+                y_pos = float((split[5])[1:-1])+((i+1)*repeat_array[3])
+                z_pos = float((split[6])[1:-1])+((i+1)*repeat_array[4])
+                new_line = (f'"{file_type}" '
+                            f'{file_name} '
+                            f'"{modulation}" '
+                            f'"{velocity}" '
+                            f'"{x_pos}" '
+                            f'"{y_pos}" '
+                            f'"{z_pos}" ')
+                position_list.append(new_line)
+    print(f'\nRepeat Position List:\n')
+    [print(L) for L in position_list]
+    return position_list
