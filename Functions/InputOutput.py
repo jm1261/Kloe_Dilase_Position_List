3ef298f909c41c6744579886092af1e4b4e35712
Josh Male <50483307+jm1261@users.noreply.github.com>
2020-01-07 09:31:50 +0000

    laser line added to output file

--- Functions/InputOutput.py
+++ Functions/InputOutput.py
@@ -12,7 +12,8 @@ def check_dir_exists(dir_path):
 
 
 def write_out_file(out_path,
-                   array):
+                   array,
+                   pos_i):
     '''
     Writes a simpled text file, txt or other extension, based on a given array
     containing strings. The array entries are written on each line.
@@ -20,6 +21,13 @@ def write_out_file(out_path,
         out_path: <string> directory path to write file
         array: <array> array containing the out file strings
     '''
+    laser_line = pos_i['laser']
+    if laser_line == 10:
+        laser_string = f'"ALS" "Ligne2/Laser1" '
+    elif laser_line == 0.5:
+        laser_string = f'"ALS" "Ligne1/Laser1" '
     output_file = open(f'{out_path}', 'w')
+    output_file.writelines('"DflVersion" "3"\n')
+    output_file.writelines(f'{laser_string}\n')
     [output_file.writelines(f'{L}\n') for L in array]
     output_file.close()
