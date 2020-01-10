19d4b86c8194689941a4a8fe4f28ab77a303b3fe
Christopher Reardon <chris@Chris-Reardons-MacBook-Pro.local>
2020-01-10 10:22:27 +0000

    PEP8 corrections, additions to gitignore

--- Position_List.py
+++ Position_List.py
@@ -10,40 +10,40 @@ position_list_dir = os.path.join(root,
                                  'Kloe')
 io.check_dir_exists(position_list_dir)
 
-## Name of the LWO file containing your pattern (single shape or design) ##
+# Name of the LWO file containing your pattern (single shape or design) ##
 lwo_file_name = 'Test.lwo'
 
-## Specific system file path, Windows requires the driver to be specified ##
-## as well as the file path. Dilase requires the entire path to file ##
+# Specific system file path, Windows requires the driver to be specified ##
+# as well as the file path. Dilase requires the entire path to file ##
 kloe_user = 'Test'
 file_name = os.path.join('D:\\',
                          'LITHO FILES',
                          f'{kloe_user}',
                          f'{lwo_file_name}')
 
-## Firstly, what should a Kloe Dilase 650 position list look like? ##
-## "ALS" "Ligne2/Laser1" ##
-## "LWO" "File Name" "Modulation" "Velocity" "x-offset" "y-offset" "z-offset" ##
+# Firstly, what should a Kloe Dilase 650 position list look like? ##
+# "ALS" "Ligne2/Laser1" ##
+# "LWO" "File Name" "Modulation" "Velocity" "x-offset" "y-offset" "z-offset" ##
 
-position_initial = {'file_name' : f'{file_name}',
-                    'laser' : 0.5,
-                    'modulation' : 0.0,
-                    'velocity' : 0.0,
-                    'x_initial' : 1,
-                    'y_initial' : 1,
-                    'z_initial' : 5.359,
+position_initial = {'file_name': f'{file_name}',
+                    'laser': 0.5,
+                    'modulation': 0.0,
+                    'velocity': 0.0,
+                    'x_initial': 1,
+                    'y_initial': 1,
+                    'z_initial': 5.359,
                     }
 
-## pattern_shift = [modulation, velocity, x, y, z, repeats] ##
-## NOTE WELL, if repeat=0 only the initial settings are set ##
-## repeat=1 yields two patterns with step sizes given below ##
+# pattern_shift = [modulation, velocity, x, y, z, repeats] ##
+# NOTE WELL, if repeat=0 only the initial settings are set ##
+# repeat=1 yields two patterns with step sizes given below ##
 pattern_shift = [10, 0, 0.27, 0, 0, 10]
 position_list = pb.position_list(pos_i=position_initial,
                                  shift_array=pattern_shift)
 
-## pattern_repeat = [modulation, velocity, x, y, z, repeats] ##
-## NOTE WELL, if repeat=0 only the initial settings are set  ##
-## repeat=1 yields two patterns with step sizes given below  ##
+# pattern_repeat = [modulation, velocity, x, y, z, repeats] ##
+# NOTE WELL, if repeat=0 only the initial settings are set  ##
+# repeat=1 yields two patterns with step sizes given below  ##
 pattern_repeat = [0, 10, 0, 0.15, 0, 10]
 position_final = pb.repeat_position_list(pos_array=position_list,
                                          repeat_array=pattern_repeat)
