2d020f81c48bef26deecdca1cd19c8b1ad1a2362
George Duffett <george.duffett@me.com>
2020-03-08 16:59:11 +0000

    Initial html, css, js

---
+++ main.js
@@ -0,0 +1,21 @@
+// Aim 1: grab and display inputs on button click
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction
+
+var defaultInputs = {
+    "filepath": "D:\\LITHO FILES",
+    "user": "Josh",
+    "filename": "Test",
+    "laser": 10,
+    "modulation": 0.0,
+    "velocity": 50,
+    "x_initial": 1,
+    "y_initial": 1,
+    "z_initial": 0,
+    "delta_mod": 10,
+    "delta_vel": 1,
+    "delta_x": 0.5,
+    "delta_y": 1,
+    "delta_z": 1,
+    "repeats": 10
+}
