2b9c0b3e5c99b75f4b5af2d6776610a37681787c
George Duffett <george.duffett@me.com>
2020-03-09 11:46:51 +0000

    Added general structure and getValues()

--- main.js
+++ main.js
@@ -1,6 +1,47 @@
-// Aim 1: grab and display inputs on button click
-
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction
+
+// $(document).ready(function () {
+//     console.log("ready!");
+//     var butt = document.getElementById("submit");
+//     butt.addEventListener('click', getValues);
+// });
+
+function makePosList(){
+    var pls = getValues();
+    var outstring = formatValues();
+    saveValues();
+}
+
+function getValues(){
+
+    var posListValues = {
+        filepath : document.getElementById('filepath').value,
+        user : document.getElementById('user').value,
+        filename : document.getElementById('filename').value,
+        laser : document.getElementById('laser').value,
+        modulation : document.getElementById('modulation').value,
+        velocity : document.getElementById('velocity').value,
+        x_initial : document.getElementById('x_initial').value,
+        y_initial : document.getElementById('y_initial').value,
+        z_initial : document.getElementById('z_initial').value,
+        delta_mod : document.getElementById('delta_mod').value,
+        delta_vel : document.getElementById('delta_vel').value,
+        delta_x : document.getElementById('delta_x').value,
+        delta_y : document.getElementById('delta_y').value,
+        delta_z : document.getElementById('delta_z').value,
+        repeats : document.getElementById('repeats').value,
+    }
+
+    return posListValues;
+};
+
+function formatValues(){
+
+}
+
+function saveValues(){
+    
+}
 
 var defaultInputs = {
     "filepath": "D:\\LITHO FILES",
