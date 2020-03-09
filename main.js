2db2e4764f879ec8f7345f94fb427f3b260e1831
George Duffett <george.duffett@me.com>
2020-03-09 17:59:12 +0000

    Outputs formatted pls to html

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 function makePosList(){
     console.log('Make pos list pressed')
     var pls = getValues();
-    var outstring = formatValues(pls);
-    saveValues();
+
+    // Select laser
+    var outstring = '';
+    if (pls.laser=='10'){
+        outstring = '"ALS" "Ligne2/Laser1"' +  '<br>';
+    } else {
+        outstring = '"ALS" "Ligne1/Laser1"' +  '<br>';
+    };
+
+    // Add parameter sweep
+    outstring += formatSweep(pls);
+    console.log(outstring);
+
+    // Output
+    document.getElementById('outDiv').innerHTML += outstring;
 }
 
 function getValues(){
@@ -12,52 +25,47 @@ function getValues(){
         user : document.getElementById('user').value,
         filename : document.getElementById('filename').value,
         laser : document.getElementById('laser').value,
-        modulation : document.getElementById('modulation').value,
-        velocity : document.getElementById('velocity').value,
-        x_initial : document.getElementById('x_initial').value,
-        y_initial : document.getElementById('y_initial').value,
-        z_initial : document.getElementById('z_initial').value,
-        delta_mod : document.getElementById('delta_mod').value,
-        delta_vel : document.getElementById('delta_vel').value,
-        delta_x : document.getElementById('delta_x').value,
-        delta_y : document.getElementById('delta_y').value,
-        delta_z : document.getElementById('delta_z').value,
-        repeats : document.getElementById('repeats').value,
+        modulation : parseFloat(document.getElementById('modulation').value),
+        velocity : parseFloat(document.getElementById('velocity').value),
+        x_initial : parseFloat(document.getElementById('x_initial').value),
+        y_initial : parseFloat(document.getElementById('y_initial').value),
+        z_initial : parseFloat(document.getElementById('z_initial').value),
+        delta_mod : parseFloat(document.getElementById('delta_mod').value),
+        delta_vel : parseFloat(document.getElementById('delta_vel').value),
+        delta_x : parseFloat(document.getElementById('delta_x').value),
+        delta_y : parseFloat(document.getElementById('delta_y').value),
+        delta_z : parseFloat(document.getElementById('delta_z').value),
+        repeats : parseInt(document.getElementById('repeats').value),
     }
 
     return posListValues;
 };
 
-function formatValues(pls){
+function formatSweep(pls){
 
-    for(var i = 0; i < (pls.repeats); i++){
-        var plsarray = [pls.filepath, pls.modulation, pls.velocity,
-            pls.x_initial, pls.y_initial, pls.z_initial];
+    var filepath = pls.filepath+'\\'+pls.filename+'.LWO';
+    var modulation = pls.modulation;
+    var velocity = pls.velocity;
+    var x_initial = pls.x_initial;
+    var y_initial = pls.y_initial;
+    var z_initial = pls.z_initial;
 
-        var positionString = '"LWO' + plsarray.join(' " ') + ' " '
-        console.log(positionString)
+    var outstring = ""
+
+    for(var i=0; i < (pls.repeats); i++){
+
+        // Format string
+        var plsarray = [filepath, modulation, velocity,
+            x_initial, y_initial, z_initial];
+        var positionString = '"LWO" "' + plsarray.join('" "') + '"' + '<br>';
+
+        // Update values
+        outstring += positionString
+        modulation += pls.delta_mod;
+        velocity += pls.delta_vel;
+        x_initial += pls.delta_x;
+        y_initial += pls.delta_y;
+        z_initial += pls.delta_z;
     }
-}
-
-function saveValues(){
-    console.log('Saving values')
-
-}
-
-var defaultInputs = {
-    "filepath": "D:\\LITHO FILES",
-    "user": "Josh",
-    "filename": "Test",
-    "laser": 10,
-    "modulation": 0.0,
-    "velocity": 50,
-    "x_initial": 1,
-    "y_initial": 1,
-    "z_initial": 0,
-    "delta_mod": 10,
-    "delta_vel": 1,
-    "delta_x": 0.5,
-    "delta_y": 1,
-    "delta_z": 1,
-    "repeats": 10
+    return outstring
 }
